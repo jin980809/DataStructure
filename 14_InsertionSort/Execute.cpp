f9ac2b761d3aa3bc73e67323e7c836db0f44875d
jin980809 <50695607+jin980809@users.noreply.github.com>
2022-03-20 17:50:19 +0900

    Add files via upload

---
+++ 14_InsertionSort/Execute.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <Windows.h>
+
+void Sort(int* datas, int length)
+{
+	for (int i = 1; i < length; i++)
+	{
+		if (datas[i - 1] <= datas[i])
+			continue;
+
+		int value = datas[i];
+		for (int k = 0; k < i; k++)
+		{
+			if (datas[k] > value)
+			{
+				memmove(&datas[k + 1], &datas[k], (i - k) * sizeof(int));
+				datas[k] = value;
+
+				break;
+			}
+		}//for(k)
+	}//for(i)
+}
+
+int main()
+{
+	int datas[] = { 5, 1, 6, 4, 2, 3 };
+	int length = sizeof(datas) / sizeof(datas[0]);
+	
+	Sort(datas, length);
+
+	for (int i = 0; i < length; i++)
+		printf("%d ", datas[i]);
+
+	printf("\n\n");
+
+	return 0;
+}
